e05fd4d10ae16736563a816e9d3c04daa446fbda
Ben Haller <github@sticksoftware.com>
2020-08-09 16:29:33 -0400

    improved R plotting script

--- plot_benchmarks.R
+++ plot_benchmarks.R
@@ -15,9 +15,18 @@ setwd("~/Desktop/SLiM-Benchmarks")
 model_files <- list.files("./models/", pattern="*.slim")
 model_names <- gsub("(.*)\\.slim", "\\1", model_files)
 
+# intersect that list with the models we have single-thread timing data for
+st_models <- list.files("./times_single/", pattern="*.txt", include.dirs=T)
+st_names <- gsub("(.*)\\.txt", "\\1", st_models)
+model_names <- intersect(model_names, st_names)
+
+# generate the model_files list for the models we will plot
+model_files <- paste0(model_names, ".slim")
+
 # find all parallel timing folders
 parallel_folders <- list.files(".", pattern="times_parallel_*", include.dirs=T)
 parallel_threads <- as.integer(gsub("times_parallel_(.*)", "\\1", parallel_folders))
+#parallel_threads <- parallel_threads[parallel_threads <= 6]		# remove hyperthreaded runs
 parallel_threads <- parallel_threads[order(parallel_threads)]
 parallel_folders <- paste0("times_parallel_", parallel_threads)
 
@@ -27,12 +36,12 @@ timing_folders <- c("times_single", parallel_folders)
 
 # plot pre-configuration
 x_range <- range(parallel_threads)
-speedup_range <- c(0, 12)
+speedup_range <- c(0, 10)
 colors <- rainbow(length(model_files), end=0.80)
 ltys <- rep(c(1,3,5), length.out=length(model_files))
 
 # make the plot from the data in the filesystem
-quartz(width=12, height=7)
+quartz(width=7, height=6)
 par(mar=c(3.1, 3.1, 2, 2), tcl=-0.3, mgp=c(1.9, 0.4, 0), family="serif")
 plot(x=x_range, y=speedup_range, type="n", xlab="Thread count", ylab="Speed relative to single-threaded", xaxt="n")
 
@@ -50,7 +59,7 @@ for (drawrep in 1:3)
 		model_color <- colors[model_index]
 		model_lty <- ltys[model_index]
 		model_x <- parallel_threads + (model_index - (length(model_files) + 1) / 2) * 0.04
-		print(model_x)
+		#print(model_x)
 		
 		model_wall_means <- NULL
 		model_wall_sems <- NULL
@@ -61,13 +70,21 @@ for (drawrep in 1:3)
 			timing_folder <- timing_folders[folder_index]
 			
 			timing_file <- paste0(timing_folder, "/", model_name, ".txt")
-			timing_data <- read.csv(timing_file)
-			wall_times <- timing_data$wall_secs
-			wall_mean <- mean(wall_times)
-			wall_sem <- sem(wall_times)
-			
-			model_wall_means <- c(model_wall_means, wall_mean)
-			model_wall_sems <- c(model_wall_sems, wall_sem)
+			if (file.exists(timing_file))
+			{
+				timing_data <- read.csv(timing_file)
+				wall_times <- timing_data$wall_secs
+				wall_mean <- mean(wall_times)
+				wall_sem <- sem(wall_times)
+				
+				model_wall_means <- c(model_wall_means, wall_mean)
+				model_wall_sems <- c(model_wall_sems, wall_sem)
+			}
+			else
+			{
+				model_wall_means <- c(model_wall_means, NA)
+				model_wall_sems <- c(model_wall_sems, NA)
+			}
 		}
 		
 		single_thread_wall_mean <- model_wall_means[1]
